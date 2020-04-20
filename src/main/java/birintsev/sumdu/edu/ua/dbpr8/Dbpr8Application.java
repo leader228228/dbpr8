82206cee658e628d11127c8904520b84c679e803
Leader <leader228228@gmail.com>
2020-04-20 18:42:43 +0300

    Initial commit

---
+++ src/main/java/birintsev/sumdu/edu/ua/dbpr8/Dbpr8Application.java
@@ -0,0 +1,13 @@
+package birintsev.sumdu.edu.ua.dbpr8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dbpr8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dbpr8Application.class, args);
+    }
+
+}
